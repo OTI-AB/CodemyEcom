df0a74901d75516c6b6609c27b0f0a583279f814
Phillip <phillip.pearson.1018@gmail.com>
2025-11-03 14:08:47 -0700

    Fixed db password 2

--- ecom/settings.py
+++ ecom/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environmental variables
-load_dotenv()
+# load_dotenv()
 
 # password DB
 DB_PASSWORD_P = os.environ['DB_PASSWORD_P']
