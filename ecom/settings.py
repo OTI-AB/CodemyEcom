7ae43e05d15424076268961a833663e9ca056585
Phillip <phillip.pearson.1018@gmail.com>
2025-11-03 08:02:36 -0700

    Initial commit after railway conversion

--- ecom/settings.py
+++ ecom/settings.py
@@ -18,6 +18,7 @@ SECRET_KEY = 'django-insecure-op)ql!!jtqn3k#ku@_g9=wrk1s=vn+wd)y8tq53jdx6$)cqm(k
 DEBUG = True
 
 ALLOWED_HOSTS = ['https://railway.com', 'localhost']
+CSRF_TRUSTED_ORIGINS = []
 
 
 # Application definition
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
     'store',
     'cart',
     "payment",
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -42,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecom.urls'
@@ -118,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = ['STATIC/']
+
+# White Noise static
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
